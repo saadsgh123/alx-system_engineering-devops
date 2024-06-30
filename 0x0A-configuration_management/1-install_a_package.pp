8077026a1eae49c2d51525ec1652f8b6c1b242e3
saadsgh123 <sdsghouri@gmail.com>
2024-06-30 20:59:48 +0100

    ieojefoijef

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
-# installs puppet-lint package
+#!/usr/bin/puppet
 
-package {'flask':
+# Install Flask version 2.1.0 using pip3
+package { 'flask':
   ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Ensure that Werkzeug is installed with version 2.0.2
+package { 'werkzeug':
+  ensure   => '2.0.2',
+  provider => 'pip3',
 }
