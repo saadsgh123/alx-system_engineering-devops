848d7725705654cbed24356e784b793c9a743db0
saadsgh123 <sdsghouri@gmail.com>
2024-06-30 22:39:11 +0100

    dheidh

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,9 +5,3 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
-
-# Ensure that Werkzeug is installed with version 2.0.2
-package { 'werkzeug':
-  ensure   => '2.0.2',
-  provider => 'pip3',
-}
