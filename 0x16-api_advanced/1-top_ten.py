88034fec5be43f8890fe29be47232622608ad82a
saadsgh123 <sdsghouri@gmail.com>
2024-08-15 20:53:36 +0100

    done ✅

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,12 +7,13 @@ import requests
 headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """method doc"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
     else:
-        return 0
+        print("None")
